6c950c8726913092bcc270925281d80ed42826c8
Neha Mehar <nehamehar31@gmail.com>
2025-05-25 03:00:58 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
     <div className="flex flex-col min-h-screen duration-500" style={{ backgroundColor: color }}>
   {/* Main content area */}
   <div className="flex-grow">
-    <div className="fixed inset-x-0 flex flex-wrap justify-center w-40 px-1 text-white h-[763px] pb-28 py-9 gap-7 bg-slate-900">
-      <h2 className="pb-6 font-semibold text-balance-xl stroke-blue-950">COLORS</h2>
-      <button onClick={() => setcolor("red")} className="px-10 py-1 bg-red-800 border border-black rounded-3xl hover:bg-slate-700">RED</button>
-      <button onClick={() => setcolor("yellow")} className="py-1 bg-yellow-700 border px-7 rounded-3xl hover:bg-slate-700">YELLOW</button>
-      <button onClick={() => setcolor("orange")} className="py-2 bg-orange-700 border px-7 rounded-3xl hover:bg-slate-700">ORANGE</button>
-      <button onClick={() => setcolor("blue")} className="px-10 py-2 border bg-sky-500 rounded-3xl hover:bg-slate-700">BLUE</button>
-      <button onClick={() => setcolor("black")} className="py-2 bg-black border px-9 rounded-3xl hover:bg-slate-700">BLACK</button>
-      <button onClick={() => setcolor("green")} className="py-2 border bg-lime-700 px-9 rounded-3xl hover:bg-slate-700">GREEN</button>
+    <div className="fixed inset-x-0 flex flex-wrap justify-center w-40 px-1 text-white h-[763px] pb-28 py-9 gap-7 bg-gray-800">
+      <h2 className="h-4 pb-6 text-2xl font-extralight">COLORS</h2>
+      <button onClick={() => setcolor("red")} className="px-10 py-2 bg-red-800 border-black rounded-3xl hover:bg-slate-700 border-s-8 border-y-4">RED</button>
+      <button onClick={() => setcolor("yellow")} className="py-1 bg-yellow-700 border-black px-7 rounded-3xl hover:bg-slate-700 border-s-8 border-y-4">YELLOW</button>
+      <button onClick={() => setcolor("orange")} className="py-2 bg-orange-700 border-black px-7 rounded-3xl hover:bg-slate-700 border-s-8 border-y-4">ORANGE</button>
+      <button onClick={() => setcolor("blue")} className="px-10 py-2 border-black bg-sky-500 rounded-3xl hover:bg-slate-700 border-s-8 border-y-4">BLUE</button>
+      <button onClick={() => setcolor("purple")} className="py-2 border-black px-9 rounded-3xl hover:bg-slate-700 border-s-8 border-y-4 bg-violet-700">PURPLE</button>
+      <button onClick={() => setcolor("green")} className="py-2 border-black bg-lime-700 px-9 rounded-3xl hover:bg-slate-700 border-s-8 border-y-4">GREEN</button>
     </div>
   </div>
 
